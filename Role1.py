7eb9c63873fb8065daa2c164704c31c904b2833b
mo <alshmoha@shernet.sheridancollege.ca>
2023-02-02 17:20:29 -0500

    logic and atrubutes for role1 and role2

--- Role1.py
+++ Role1.py
@@ -1,1 +1,15 @@
 """logic for paladin role"""
+import random
+
+def create_paladin(name, speed, defense):
+    return {
+        'name': name,
+        'speed': speed,
+        'defense': defense
+    }
+
+def attack(paladin):
+    return paladin['speed'] + random.randint(1, 12)
+
+def defend(paladin, attack_strength):
+    return paladin['defense'] + random.randint(1, 12)
--- Role2.py
+++ Role2.py
@@ -1,1 +1,15 @@
 """logic for necromancer role"""
+import random
+
+def create_necromancer(name, magic, defense):
+    return {
+        'name': name,
+        'magic': magic,
+        'defense': defense
+    }
+
+def cast_spell(necromancer):
+    return necromancer['magic'] + random.randint(1, 12)
+
+def defend(necromancer, attack_strength):
+    return necromancer['defense'] + random.randint(1, 12)
